ce6c00b2caf1d9f56b4a213627ea015a8c6160bb
ChenfeiHome <643487053@qq.com>
2018-06-26 06:22:36 +0800

    @Bean,@Autowired,@Qualifier,@Primary,@Resource,@Inject,

--- src/main/java/com/ntc/lesson4/cap9/config/Cap9MainConfig.java
+++ src/main/java/com/ntc/lesson4/cap9/config/Cap9MainConfig.java
@@ -3,6 +3,7 @@ package com.ntc.lesson4.cap9.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.ntc.lesson4.cap9.dao.TestDao;
 
@@ -14,8 +15,29 @@ import com.ntc.lesson4.cap9.dao.TestDao;
 })
 public class Cap9MainConfig {
 	
+	/*
+	 * @Autowired 和  @Bean 同时存在时,
+	 * 注册bean是有优先级的，
+	 * 优先@Autowired，获取默认的类，
+	 * 并且不再执行@Bean，
+	 * 如果要执行指定的@Bean，需在@Autowired上使用@Qualifier指明bean的name
+	 */
+//	@Bean("testDao2")
+//	public TestDao testDao(){
+//		 TestDao testDao = new TestDao();
+//		 testDao.setFlag("2");
+//		 return testDao;
+//	}
+	
+	/*
+	 * @Primary优先级较高（首选项）
+	 * Spring进行自装配的时候默认首选的bean
+	 */
+	@Primary
 	@Bean("testDao2")
 	public TestDao testDao(){
-		return new TestDao();
+		 TestDao testDao = new TestDao();
+		 testDao.setFlag("2");
+		 return testDao;
 	}
 }
--- src/main/java/com/ntc/lesson4/cap9/service/TestService.java
+++ src/main/java/com/ntc/lesson4/cap9/service/TestService.java
@@ -1,6 +1,8 @@
 package com.ntc.lesson4.cap9.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Service;
 
 import com.ntc.lesson4.cap9.dao.TestDao;
@@ -8,7 +10,33 @@ import com.ntc.lesson4.cap9.dao.TestDao;
 @Service
 public class TestService {
 
-	@Autowired
+	/*
+	 * @Autowired 和  @Bean 同时存在时,
+	 * 注册bean是有优先级的，
+	 * 优先@Autowired，获取默认的类，
+	 * 并且不再执行@Bean，
+	 * 如果要执行指定的@Bean，需在@Autowired上使用@Qualifier指明bean的name
+	 */
+//	@Qualifier("testDao2")
+//	@Autowired(required = false)
+	
+	/*
+	 * 效果是一样的，与Autowired效果一样可以装备bean（可以指定名称，也可以不指定名称）
+	 * 1.不支持@Primary功能(不支持优先级)
+	 * 2.不支持@Autowired(required = false)(当bean不存在时，Resource会报错)
+	 * 建议使用@Autowired
+	 */
+//	@Resource(name="testDao")
+	
+	/*
+	 * 需要pom.xml额外引用javax.inject，
+	 * 和@Autowired功能差不多，
+	 * 支持@Primary，
+	 * 当bean不存在时，也不会报错，
+	 * 只是没有Autowired(required = false)功能	
+	 * 有些项目不是基于spring，可以使用@Inject(第三方，不依赖spring)
+	 */
+	@Inject
 	private TestDao testDao;
 	
 	public void println(){
